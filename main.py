64f2fe165c31dd866d352d3c697e4ede7ed81095
pmutua <pmutua@live.com>
2023-06-23 19:01:31 +0300

    feat: add lambda function handler

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from config.database import Base
 from config.database import engine
 from routers import document_router, user_router
 from fastapi import FastAPI
+from mangum import Mangum
 
 
 Base.metadata.create_all(bind=engine)
@@ -10,4 +11,6 @@ app = FastAPI()
 
 app.include_router(document_router.router)
 app.include_router(user_router.router)
+
+handler = Mangum(app=app)
 
