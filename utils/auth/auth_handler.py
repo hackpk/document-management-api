bbe4d6bf2ce34b54280076ff538d1734474e9785
pmutua <pmutua@live.com>
2023-06-23 14:32:05 +0300

    feat: add helper functions for auth (#4)

--- utils/auth/auth_handler.py
+++ utils/auth/auth_handler.py
@@ -1,4 +1,8 @@
+from fastapi import Depends
 from passlib.context import CryptContext
+from fastapi.security import OAuth2PasswordBearer
+from services.jwt_service import JWTService
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login")
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -9,3 +13,6 @@ def verify_password(plain_password, hashed_password):
 
 def get_password_hash(password):
     return pwd_context.hash(password)
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    return JWTService.decode_access_token(token)
