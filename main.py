79ee799002106850f5ba5885f2fce81a7ca7b5b8
pmutua <pmutua@live.com>
2023-06-23 14:34:15 +0300

    feat: expose use and document routes (#18)

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-from fastapi import FastAPI
 from config.database import Base
 from config.database import engine
 from routers import document_router, user_router
+from fastapi import FastAPI
+
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-# app.include_router(document_router.router)
-# app.include_router(user_router.router)
+app.include_router(document_router.router)
+app.include_router(user_router.router)
 
